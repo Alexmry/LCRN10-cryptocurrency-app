f1d458ccc0fc7058438e9f1aa91375e057245fec
Alexandre Morey <alexander006_9@hotmail.com>
2022-04-10 21:08:40 +0100

    nav bar

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -11,6 +11,31 @@ import { COLORS, FONTS, icons } from "../constants";
 
 const Tab = createBottomTabNavigator();
 
+const TabBarCustomButton = ({ children, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        top: -30,
+        justifyContent: "center",
+        alignItems: "center",
+        ...styles.shadow
+      }}
+      onPress={onPress}
+    >
+      <LinearGradient
+        colors= {[COLORS.primary, COLORS.secondary]}
+        style={{
+          width: 70,
+          height: 70,
+          borderRadius: 35
+        }}
+      >
+        {children}
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+};
+
 const Tabs = () => {
   return (
     <Tab.Navigator
@@ -98,7 +123,7 @@ const Tabs = () => {
                 />
             ),
             tabBarButton: (props) => (
-                <tabBarCustomButton
+                <TabBarCustomButton
                     {...props}
                 />
             )
