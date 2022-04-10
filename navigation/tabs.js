f69720a7e9eac202854c9955bb6f4b0d7346cd18
Alexandre Morey <alexander006_9@hotmail.com>
2022-04-10 20:06:42 +0100

    nav bar

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -16,60 +16,51 @@ const Tab = createBottomTabNavigator()
 
 const Tabs = () => {
     return (
-        <Tab.Navigator
-            tabBarOptions={{
-                showLabel: false,
-                style: {
-                    postions: 'absolute',
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    elevation: 0,
-                    backgroundColor: COLORS.white,
-                    borderTopColor: "transparent",
-                    height: 100
-                }
-            }}  
-
-        >
-            <Tab.Screen
-                name="Home"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Portfolio"
-                component={Home}
-                options= {{
-                    tabBarIcon: ({focused}) => (
-                        <View sytle = {{alignItems: 'center', justifyContent: 'center'}}>
-                            <Image
-                                source={icons.home}
-                                resizeMode="contain"
-                                style = {{
-                                    width: 30,
-                                    height: 30,
-                                    tintColor: focused ? COLORS.primary : COLORS.black
-                                }}
-                            />
-                            <Text></Text>
-                        </View>
-                    )
-                }}
-            />
-            <Tab.Screen
-                name="Transaction"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Prices"
-                component={Home}
-            />
-            <Tab.Screen
-                name="Settings"
-                component={Home}
-            />
-        </Tab.Navigator>
-    )
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: false,
+          style: {
+            postions: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            elevation: 0,
+            backgroundColor: COLORS.white,
+            borderTopColor: "transparent",
+            height: 100,
+          },
+        }}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen
+          name="Portfolio"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View sytle={{ alignItems: "center", justifyContent: "center" }}>
+                <Image
+                  source={icons.home}
+                  resizeMode="contain"
+                  style={{
+                    width: 30,
+                    height: 30,
+                    tintColor: focused ? COLORS.primary : COLORS.black,
+                  }}
+                />
+                <Text
+                  style={{ color: focused ? COLORS.primary : COLORS.black, ...FONTS.body5 }}
+                >
+                  HOME
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen name="Transaction" component={Home} />
+        <Tab.Screen name="Prices" component={Home} />
+        <Tab.Screen name="Settings" component={Home} />
+      </Tab.Navigator>
+    );
 }
 
 const styles = StyleSheet.create({
