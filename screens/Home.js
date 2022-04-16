4b7dc6fa8a3ffc2fc48673aedb0dd7520e7511d1
Alexandre Morey <alexander006_9@hotmail.com>
2022-04-16 11:57:24 +0100

    home page background image

--- screens/Home.js
+++ screens/Home.js
@@ -18,20 +18,44 @@ const Home = ({ navigation }) => {
 
     function renderHearder() {
 
-        const renderItem = ({item, index}) => (
-            <TouchableOpacity
-                style= {{
-                    width: 180,
-                    paddingVertical: SIZES.padding,
-                    paddingHorizontal: SIZES.padding,
-                    marginLeft: index == 0 ? SIZES.padding : 0,
-                    marginRight: SIZES.radius,
-                    borderRadius: 10,
+        const renderItem = ({ item, index }) => (
+          <TouchableOpacity
+            style={{
+              width: 180,
+              paddingVertical: SIZES.padding,
+              paddingHorizontal: SIZES.padding,
+              marginLeft: index == 0 ? SIZES.padding : 0,
+              marginRight: SIZES.radius,
+              borderRadius: 10,
+              backgroundColor: COLORS.white,
+            }}
+          >
+            {/* currency */}
+            <View style={{ flexDirection: "row" }}>
+              <View>
+                <Image
+                  source={item.image}
+                  resizeMode="cover"
+                  style={{
+                    marginTop: 5,
+                    width: 25,
+                    height: 25,
+                  }}
+                />
+              </View>
+              <View
+                style={{
+                  marginLeft: SIZES.base,
                 }}
-            >
+              >
+                <Text style={{...FONTS.h2}}>{item.currency}</Text>
+                <Text style={{color: COLORS.gray, ...FONTS.body3}}>{item.code}</Text>
+              </View>
+            </View>
 
-            </TouchableOpacity>
-        )
+            {/* value */}
+          </TouchableOpacity>
+        );
 
         return (
           <View
